5ab8ba71204b4f5bb194875514e259660a4deaa5
erichustle <96139491+erichustle@users.noreply.github.com>
2021-12-14 19:33:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("user name");
+ var b=prompt("user Password");
+  	if(a== ".........."&&b==".............")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
